eda1c34199f1e96e08d9ba09a00184f7180fb910
Evgeniy <e.pogrebnyak@gmail.com>
2021-01-22 18:59:59 +0300

    function to embed tweets

---
+++ _README.md
@@ -0,0 +1,28 @@
+## Geospatial
+
+#### Night GDP
+
+https://twitter.com/thesamasher/status/1352359375155859456
+
+
+## Freelance
+
+#### Problem scope
+
+https://twitter.com/Suhail/status/1352321633701122048
+
+## Back of napkin
+
+https://twitter.com/PogrebnyakE/status/1352546296347971584
+
+## Econometrics
+
+https://twitter.com/Alessia_metrics/status/1352296926578937857
+
+## ML
+
+### "Flat-earth" ML
+
+https://twitter.com/josh_tobin_/status/1352350403304677376
+
+
---
+++ convert.py
@@ -0,0 +1,46 @@
+from dataclasses import dataclass
+from pathlib import Path
+import requests
+
+URL = "https://twitter.com/{name}/status/{id}"
+API = "https://publish.twitter.com/oembed?url={url}"
+
+@dataclass
+class Tweet:
+  """Original code: https://bit.ly/3p9kI0T"""
+  name: str
+  id: int
+
+  @property
+  def url(self):
+    return URL.format(name=self.name, id=self.id)
+
+  @property
+  def api_url(self):
+    # Twitter's oEmbed API  https://dev.twitter.com/web/embedded-tweets
+    return API.format(url=self.url)
+
+  def html(self):
+    return requests.get(self.api_url).json()["html"]
+
+  def _repr_html_(self):
+     return self.html() 
+
+def mk_tweet(s: str) -> Tweet:
+  a = s.strip().split("/")
+  ix = a.index("status")
+  return Tweet(a[ix-1], int(a[ix+1]))
+  
+def to_list(doc: str):
+  return doc.split("\n")
+
+def substitute_tweets(line: str) -> str:
+   if line.startswith("http") and ("status" in line):
+     return mk_tweet(line).html()
+   else:
+     return line
+
+doc = Path("_README.md").read_text()
+lines = map(substitute_tweets, to_list(doc))
+text = "\n".join(lines) 
+Path("README.md").write_text(text, encoding="utf-8")     
