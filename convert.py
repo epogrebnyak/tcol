dcceec0ef184db98199ee20a0ebc7c002862ec3b
Evgeniy <e.pogrebnyak@gmail.com>
2021-01-24 10:41:10 +0300

    Update convert.py

--- convert.py
+++ convert.py
@@ -1,3 +1,7 @@
+"""Convert twitter URLs in markdown file to HTML cards
+
+Also sorts H1 headers.
+"""
 from dataclasses import dataclass
 from pathlib import Path
 
@@ -8,7 +12,7 @@ URL = "https://twitter.com/{name}/status/{id}"
 API = "https://publish.twitter.com/oembed?url={url}"
 
 import requests_cache
-requests_cache.install_cache("tweets.sqlite")
+requests_cache.install_cache("tweets")
 
 @dataclass
 class Tweet:
@@ -24,6 +28,7 @@ class Tweet:
     @property
     def api_url(self):
         # Twitter's oEmbed API  https://dev.twitter.com/web/embedded-tweets
+        print("Processing", self)
         return API.format(url=self.url)
 
     def html(self):
@@ -34,6 +39,13 @@ class Tweet:
 
 
 def mk_tweet(s: str) -> Tweet:
+    """Constructor for Tweet instance.
+    
+    Parameters
+    ----------
+    s : str
+        twitter URL, eg "https://twitter.com/PHuenermund/status/1352909842118291457"
+    """
     a = s.strip().split("/")
     ix = a.index("status")
     return Tweet(a[ix - 1], int(a[ix + 1]))
@@ -44,42 +56,52 @@ def to_list(doc: str):
 
 
 def substitute_tweets(line: str) -> str:
+    """Change tweet url to html."""
     if line.startswith("http") and ("status" in line):
         return mk_tweet(line).html()
     else:
         return line
 
-
-def main(source_file="_README.md", destination_file="README.md"):
-    # read and sort blocks
-    doc = Path(source_file).read_text()
-    lines = to_list(doc)
-    groups = [group for group in yield_groups(lines) if group]
-    groups = sorted(groups, key = lambda xs: xs[0])
-    lines = [line for group in groups for line in group]
-    # create toc via temp file
-    text = "\n".join(lines)
-    Path("tempfile.txt").write_text(text, encoding="utf-8")    
-    toc = md_toc.build_toc("tempfile.txt")
-    # make cards
-    lines = list(map(substitute_tweets, lines))    
-    # save
-    body = "\n".join(lines)    
-    text = "\n".join([toc, body])
-    Path(destination_file).write_text(text, encoding="utf-8")
-    return lines
-
-
-def yield_groups(lines):
+def yield_groups(lines, mark="# "):
     group = []
     for line in lines:
-        if line.startswith("# "):
+        if line.startswith(mark):
             yield group
             group = [line]
         else:
             group += [line]
+
+def sort_blocks(doc):
+    """Sort H1 header blocks alphabetically."""
+    lines = to_list(doc)
+    groups = [group for group in yield_groups(lines) if group]
+    groups = sorted(groups, key = lambda xs: xs[0])
+    lines = [line for group in groups for line in group]
+    return "\n".join(lines)
+
     
+def create_output(doc: str):    
+    # create toc via temp file (that is how md_toc.build_toc works)
+    text = sort_blocks(doc)
+    Path("tempfile.txt").write_text(text, encoding="utf-8")    
+    toc = md_toc.build_toc("tempfile.txt")
+    
+    # make cards
+    lines = to_list(doc)
+    lines = list(map(substitute_tweets, lines))    
+    
+    # combine toc and body
+    body = "\n".join(lines)    
+    return "\n".join([toc, body])
+
+    
+def main(source_file="_README.md", destination_file="README.md"):    
+    doc = Path(source_file).read_text()
+    text = create_output(doc)
+    Path(destination_file).write_text(text, encoding="utf-8")
+    return doc, text
+
 
 if __name__ == "__main__":
-    lines = main()
+    lines = main(source_file="_README.md", destination_file="README.md")
     
